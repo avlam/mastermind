d70870c74835a14513e78d534f3b359ea0913bae
Hee Kang <kanglm32@gmail.com>
2018-03-31 11:09:37 -0700

    due to angela making me do this

--- static/plots.js
+++ static/plots.js
@@ -116,3 +116,104 @@ function histoPlot(){
     
         Plotly.newPlot('speed', data, layout);
     };
+
+    function histotwo() {
+        Plotly.d3.json(url, function(error, response){
+            if (error) return console.warn(error);
+            console.log(response)
+
+        var color = "steelblue";
+
+        // Generate a 1000 data points using normal distribution with mean=20, deviation=5
+        // var values = d3.range(1000).map(d3.random.normal(20, 5));
+        var games = response.game;
+        var guesses = response.guess_number;
+        var gameswon = Object.keys(games).map(function (key) {
+             return games[key];
+             })
+             var values = Object.keys(guesses).map(function (key) {
+                return guesses[key];
+                })
+            console.log(gameswon);
+            console.log(values); 
+
+        last_player = values[values.length - 1];
+        console.log(last_player);
+
+        // A formatter for counts.
+        var formatCount = d3.format(",.0f");
+
+        var margin = {top: 20, right: 300, bottom: 30, left: 30},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var max = d3.max(values);
+        var min = d3.min(values);
+        var x = d3.scale.linear()
+            .domain([0, 20])
+            // .domain([min, max])
+            .range([0, width]);
+
+        // Generate a histogram using twenty uniformly-spaced bins.
+        var data = d3.layout.histogram()
+            .bins(x.ticks(20))
+            (values);
+
+        var yMax = d3.max(data, function(d){return d.length});
+        var yMin = d3.min(data, function(d){return d.length});
+        var colorScale = d3.scale.linear()
+                    .domain([yMin, yMax])
+                    .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+
+        var y = d3.scale.linear()
+            .domain([0, yMax])
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .tickValues(d3.range(0, 21, 1))    
+            .scale(x)
+            .orient("bottom");
+
+        var svg = d3.select("#plot").append("svg")
+        // var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            // .attr("width", width)
+            // .attr("height", height)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var bar = svg.selectAll(".bar")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(" + (x(d.x)-15) + "," + y(d.y) + ")"; });
+            // .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+        bar.append("rect")
+            .attr("x", 1)
+            .attr("width", (x(data[0].dx) - x(0)) - 1)
+            .attr("height", function(d) { return height - y(d.y); })
+
+            // .attr("fill", function(d) { return colorScale(d.y) });
+            .attr("fill", function(d) {console.log(d)
+                console.log(d.x);
+                if (d.x === last_player) {
+                return "red";
+              } else  {
+                return colorScale(d.y);}
+             
+            });
+
+        bar.append("text")
+            .attr("dy", ".75em")
+            .attr("y", -12)
+            .attr("x", (x(data[0].dx) - x(0)) / 2)
+            .attr("text-anchor", "middle")
+            .text(function(d) { return formatCount(d.y); });
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+            })};
