233bcaa2534ab0e5bc7322feca9e90f6e185423b
minas26902 <angeladetweiler@gmail.com>
2018-03-31 11:17:22 -0700

    more dial code

--- static/plots.js
+++ static/plots.js
@@ -10,51 +10,119 @@ var url = "http://ucbe-mastermind.herokuapp.com/by_game/columns"
 //             console.log(response[key])
 //         }
 //     }});
-function histoPlot(){
-    Plotly.d3.json(url, function(error, response){
-    if (error) return console.warn(error);
-    console.log(response)
-    var games = response.game;
-    var guesses = response.guess_number;
-    var gameswon = Object.keys(games).map(function (key) {
-         return games[key];
-         })
-         var num_guess = Object.keys(guesses).map(function (key) {
-            return guesses[key];
-            })
-        console.log(gameswon);
-        console.log(num_guess); 
+// function histoPlot(){
+//     Plotly.d3.json(url, function(error, response){
+//     if (error) return console.warn(error);
+//     console.log(response)
+//     var games = response.game;
+//     var guesses = response.guess_number;
+//     var gameswon = Object.keys(games).map(function (key) {
+//          return games[key];
+//          })
+//          var num_guess = Object.keys(guesses).map(function (key) {
+//             return guesses[key];
+//             })
+//         console.log(gameswon);
+//         console.log(num_guess); 
 
-    var trace = {
-            x: num_guess,
-            type: 'histogram',
-            xbins: {
-                end: 20, 
-                size: 1, 
-                start: 0
-              },
-          };
-    var layout = {
-  bargap: 0.05, 
-  bargroupgap: 0.2, 
-  barmode: "overlay", 
-  title: "Histogram",
-  xanchor: 'center', 
-  xaxis: {
-    autotick: false,
-    ticks: 'outside',
-    tick0: 0,
-    dtick: 1,
-    ticklen: 5,
-    tickwidth: 3,
-    tickcolor: '#000'
-  } , 
-  yaxis: {title: "Number of Games"}
-}
-        var data = [trace];
-        Plotly.newPlot('plot', data, layout);
+//     var trace = {
+//             x: num_guess,
+//             type: 'histogram',
+//             xbins: {
+//                 end: 20, 
+//                 size: 1, 
+//                 start: 0
+//               },
+//           };
+//     var layout = {
+//   bargap: 0.05, 
+//   bargroupgap: 0.2, 
+//   barmode: "overlay", 
+//   title: "Histogram",
+//   xanchor: 'center', 
+//   xaxis: {
+//     autotick: false,
+//     ticks: 'outside',
+//     tick0: 0,
+//     dtick: 1,
+//     ticklen: 5,
+//     tickwidth: 3,
+//     tickcolor: '#000'
+//   } , 
+//   yaxis: {title: "Number of Games"}
+// }
+//         var data = [trace];
+//         Plotly.newPlot('plot', data, layout);
 
-    })};
+//     })};
+
+    function pinGauge(pin){
+    // Link to pin_difficulty lookup to pull frequency 
+    url = `/lookup_difficulty/${pin}`
+    Plotly.d3.json(url, function(error, response) {
+        if (error){return console.warn(error);}
+            var frequency = response.frequency;	
+            var level = response.level;
+                              
+            console.log('lookup_difficulty/pin:',response);
+    
+        // Trig to calc meter point
+        var degrees = (180/255) * frequency,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+    
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    
+        var data = [{ type: 'scatter',
+        x: [0], y:[0],
+            marker: {size: 28, color:'850000'},
+            showlegend: false,
+            name: 'pin_diff',
+            text: level,
+            hoverinfo: 'text+name'},
+        { values: [50/3, 50/3, 50/3,50],
+        rotation: 90,
+        text: ['Rare', 'Uncommon', 'Common', ''],
+        textinfo: 'text',
+        textposition:'inside',      
+        marker: {colors:['rgba(255, 0, 0, .9)', 'rgba(255, 190, 0, .9)',
+                                'rgba(255, 255, 0, .9)', 'rgba(255, 255, 255, 0)']},
+        //labels: ['151-180', '121-150', '91-120',''],
+        //hoverinfo: 'label',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
+        }];
+    
+        var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+                color: '850000'
+            }
+            }],
+        title: '<b>Pin Difficulty Gauge</b>',
+        height: 700,
+        width: 900,
+        xaxis: {zeroline:false, showticklabels:false,
+                    showgrid: false, range: [-1, 1]},
+        yaxis: {zeroline:false, showticklabels:false,
+                    showgrid: false, range: [-1, 1]}
+        };
+    
+        Plotly.newPlot('pin_diff', data, layout);
+    
+})}
 
     function speedGauge(){
         // Enter a speed between 0 and 180
